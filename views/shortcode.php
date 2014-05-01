af6fa62fae22309e5290d35c1a8b2450687e9bd9
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-01 12:15:10 +0000

    New Version Release and Tagging Old Version
    git-svn-id: https://plugins.svn.wordpress.org/contact-bank/tags/2.0.21@906257 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/shortcode.php
@@ -0,0 +1,19 @@
+<form id="shortcode" class="layout-form" method="post">
+	<div id="poststuff" style="width: 99% !important;">
+		<div id="post-body" class="metabox-holder">
+			<div id="postbox-container-2" class="postbox-container">
+				<div id="advanced" class="meta-box-sortables">
+					<div id="contact_bank_get_started" class="postbox" >
+						<div class="handlediv" data-target="#ux_shortcode" title="Click to toggle" data-toggle="collapse"><br></div>
+						<h3 class="hndle"><span><?php _e("How to setup Short-Codes for Contact Bank into your WordPress Page/Post?", contact_bank); ?></span></h3>
+						<div class="inside">
+							<div id="ux_shortcode" class="contact_bank_layout">
+								<img src="<?php echo CONTACT_BK_PLUGIN_URL . "/assets/images/how-to-setup-short-code-cb.png";?>" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
