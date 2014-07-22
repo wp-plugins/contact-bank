316afd8ca05b9835090eea3996fe0d503ce5a58b
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-22 04:16:06 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/contact-bank/tags/2.0.49@952572 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/shortcode.php
@@ -0,0 +1,52 @@
+<?php 
+global $wpdb,$current_user,$cb_user_role_permission;
+$cb_role = $wpdb->prefix . "capabilities";
+$current_user->role = array_keys($current_user->$cb_role);
+$cb_role = $current_user->role[0];
+switch($cb_role)
+{
+	case "administrator":
+		$cb_user_role_permission = "manage_options";
+		break;
+	case "editor":
+		$cb_user_role_permission = "publish_pages";
+		break;
+	case "author":
+		$cb_user_role_permission = "publish_posts";
+		break;
+	case "contributor":
+		$cb_user_role_permission = "edit_posts";
+		break;
+	case "subscriber":
+		$cb_user_role_permission = "read";
+		break;
+}
+if (!current_user_can($cb_user_role_permission))
+{
+	return;
+}
+else
+{
+?>
+<form id="shortcode" class="layout-form" method="post">
+	<div id="poststuff" style="width: 99% !important;">
+		<div id="post-body" class="metabox-holder">
+			<div id="postbox-container-2" class="postbox-container">
+				<div id="advanced" class="meta-box-sortables">
+					<div id="contact_bank_get_started" class="postbox" >
+						<div class="handlediv" data-target="#ux_shortcode" title="Click to toggle" data-toggle="collapse"><br></div>
+						<h3 class="hndle"><span><?php _e("How to setup Short-Codes for Contact Bank into your WordPress Page/Post?", contact_bank); ?></span></h3>
+						<div class="inside">
+							<div id="ux_shortcode" class="contact_bank_layout">
+								<img src="<?php echo CONTACT_BK_PLUGIN_URL . "/assets/images/how-to-setup-short-code-cb.png";?>" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
+<?php 
+}
+?>
