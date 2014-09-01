d33f4663f9895d92933cd671fba3c785be4a1cef
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-01 05:05:34 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/contact-bank/trunk@976250 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contact-bank.php
+++ contact-bank.php
@@ -4,7 +4,7 @@ Plugin Name: Contact Bank Standard Edition
 Plugin URI: http://tech-banker.com
 Description: Build Complex, Powerful Contact Forms in Just Seconds. No Programming Knowledge Required! Yeah, It's Really That Easy.
 Author: Tech Banker
-Version: 2.0.61
+Version: 2.0.62
 Author URI: http://tech-banker.com
  */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
